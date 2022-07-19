86ec8e4ac7cd23a7894f16c3cf80f23f8664a929
Puteri Husnul Khotimah <husnulputeri@gmail.com>
2022-07-19 17:23:40 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -2,6 +2,14 @@
 // File System
 const fs = require("fs");
 
-// menuliskan string ke file (synchronous)
-fs.writeFileSync("test.txt", "Hello World secara synchronous!");
-// () yang 1 nama file, yang ke 2 tulisan/isi dari file nya
+// // menuliskan string ke file (synchronous)
+// fs.writeFileSync("test.txt", "Hello World secara synchronous!");
+// // () yang 1 nama file, yang ke 2 tulisan/isi dari file nya
+
+// Menampung Error menggunakan blok try catch
+try {
+  fs.writeFileSync("data/test.txt", "Hello World secara synchronous!");
+} catch (e) {
+  console.log(e);
+}
+//   ini error karena tidak ada folder data. jikapun ada, maka tidak error
