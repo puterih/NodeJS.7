f16038bae3976d392c70ed342d7e20a30c8f95a3
Puteri Husnul Khotimah <husnulputeri@gmail.com>
2022-07-19 18:26:42 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -19,12 +19,19 @@ const fs = require("fs");
 //   console.log(err); // null karena fungsi ini tidak meng return apapun
 // });
 
-// Membaca isi file (synchronous)
-const data = fs.readFileSync("data/test.txt", "utf-8");
-// console.log(data); //<Buffer 48 65 6c 6c 6f 20 57 6f 72 6c 64 20 73 65 63 61 72 61 20 41 73 79 6e 63 72 6f 6e 6f 75 73>
-// yang tampil adalah Buffernya, bukan data string. agar berubah menjadi string, pakai functiong toString
+// // Membaca isi file (synchronous)
+// const data = fs.readFileSync("data/test.txt", "utf-8");
+// // console.log(data); //<Buffer 48 65 6c 6c 6f 20 57 6f 72 6c 64 20 73 65 63 61 72 61 20 41 73 79 6e 63 72 6f 6e 6f 75 73>
+// // yang tampil adalah Buffernya, bukan data string. agar berubah menjadi string, pakai functiong toString
 
-// console.log(data.toString());
-// // jika menggunakan encoding "utf-8" maka tidak perlu menggunakan toString
-// // penggunaan encoding "utf-8" contohnya untuk mengubah yang tadinya Buffer menjadi huruf latin
-console.log(data);
+// // console.log(data.toString());
+// // // jika menggunakan encoding "utf-8" maka tidak perlu menggunakan toString
+// // // penggunaan encoding "utf-8" contohnya untuk mengubah yang tadinya Buffer menjadi huruf latin
+// console.log(data);
+
+// Membaca isi file (Asynchronous) *gaya penulisan callback
+fs.readFile("data/test.txt", "utf-8", (err, data) => {
+  //(err, data) merupakan 2 callback. Data itu yg dikembalikan dari isi filenya
+  if (err) throw err; //throw(melemparkan) sama seperti return karna langsung keluar dari function
+  console.log(data); // jika berhasil
+});
